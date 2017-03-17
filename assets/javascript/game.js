c08f969941c065a17db2289435b3cd94df35a303
Baggio Shehadi <k3nh1n@yahoo.com>
2017-03-17 17:02:48 -0500

    got rid of comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,41 +1,4 @@
 $(document).ready(function() {
-
-    // CREATE THE MISSING CODE HERE. Your code should add content to the random-number div.
-    // ...
-    //   var word1 = ["e","l","e","p","h","a","n","t"];
-    //   var userguess = "z"
-    //   var lengtharray = word1.length;
-    //   var test = [];
-    //   var letters = [];
-    //   var wrong = 0;
-    //   var testindex
-    //   var checkwrong = false;
-
-    //   for (var i = 0; i < word1.length; i++) {
-    //     test.push("_");
-    //   }
-    //   // console.log(test);
-    //   for (var i = 0; i < word1.length; i++) {
-    //     if(word1[i]===userguess){
-    //       test[i] = userguess;
-    //       if(letters.indexOf(userguess)==-1){
-    //         letters.push(userguess);
-    //     }
-    //   }
-    //     else{
-    //       checkwrong = true;
-    //       if(letters.indexOf(userguess)==-1){
-    //       letters.push(userguess);
-    //     }
-    //   }
-    // }
-    // if(checkwrong){
-    //   wrong++
-    // }
-    //   console.log(letters);
-    //   console.log(test);
-    //   console.log(wrong);
-    //   // ...
     function pushit(x) {
         if (x.indexOf(userguess) == -1) {
             x.push(userguess);
@@ -72,10 +35,6 @@ $(document).ready(function() {
     var wincounter = 0;
     var wins = 0;
     var rightletters = [];
-    // console.log(randomnumber);
-    // console.log(words[randomnumber]);
-    // var checkwrong = true;
-    // console.log(words[0][2]);
     for (var i = 0; i < words[randomnumber].length; i++) {
         usersee.push("_");
         userseediv = usersee.join(" ");
@@ -86,7 +45,6 @@ $(document).ready(function() {
         var checkwrong = true;
         // Captures the key press, converts it to lowercase, and saves it to a variable.
         userguess = String.fromCharCode(event.keyCode).toLowerCase();
-        // console.log(wincounter);
         if (letters.indexOf(userguess) >= 0) {} else {
 
             if (event.keyCode >= 65 && event.keyCode <= 90 && wrong < 6 && wincounter < words[randomnumber].length) {
@@ -96,15 +54,8 @@ $(document).ready(function() {
                         userseediv = usersee.join(" ");
                         checkwrong = false;
                         wincounter++;
-                        // console.log(userseediv);
-                        // if (letters.indexOf(userguess) == -1) {
-                        //     letters.push(userguess);
-                        // }
                         pushit(letters);
                     } else {
-                        // if (letters.indexOf(userguess) == -1) {
-                        //     letters.push(userguess);
-                        // }
                         pushit(letters);
                     }
                 }
@@ -114,25 +65,6 @@ $(document).ready(function() {
                         wrong++
                     }
                 }
-                // if (wrong === 1) {
-                //     $("#wrongcounter").attr("src", "assets/images/hangwrong1.gif")
-                // }
-                // if (wrong === 2) {
-                //     $("#wrongcounter").attr("src", "assets/images/hangwrong2.gif")
-                // }
-                // if (wrong === 3) {
-                //     $("#wrongcounter").attr("src", "assets/images/hangwrong3.gif")
-                // }
-                // if (wrong === 4) {
-                //     $("#wrongcounter").attr("src", "assets/images/hangwrong4.gif")
-                // }
-                // if (wrong === 5) {
-                //     $("#wrongcounter").attr("src", "assets/images/hangwrong5.gif")
-                // }
-                // if (wrong === 6) {
-                //     alert("Sorry you lose press reset to get a new word");
-                //     $("#wrongcounter").attr("src", "assets/images/hangwrong6.gif")
-                // }
                 $("#wrongcounter").attr("src", "assets/images/hangwrong" + wrong + ".gif");
                 if (wrong === 6) {
                     alert("Sorry you lose. Press reset for another word.")
@@ -144,12 +76,6 @@ $(document).ready(function() {
                 }
                 $("#usersee").html("<h1>" + userseediv + "</h1>");
                 $("#lettersused").html("<h2>" + letters + "</h2>");
-                // console.log(wincounter);
-                // console.log(userguess);
-                // console.log(words);
-                // console.log(letters);
-                // console.log(usersee);
-                // console.log(wrong);
             }
         }
 
